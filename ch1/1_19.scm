cbedce02747fc9fc6c49e1e7ec1a47695221dd89
dsh <dshbusiness@gmail.com>
2018-01-31 23:45:29 +0800

    x

--- ch1/1_19.scm
+++ ch1/1_19.scm
@@ -34,3 +34,20 @@
 
 (display (fib 8))
 (newline)
+
+; 习题 1.20
+; 正则序求值，reminder被调用18次
+; 调用序列如下，if后的#x表示为了求if的条件部分，需要调用reminder的部分
+; (gcd 206 40)
+; (gcd 40 (reminder 206 40))
+; if #1
+; (gcd (reminder 206 40) (reminder 40 (reminder 206 40)))
+; if #2
+; (gcd (reminder 40 (reminder 206 40)) (reminder (reminder 206 40) (reminder 40 (reminder 206 40))))
+; if #4
+; (gcd (reminder (reminder 206 40) (reminder 40 (reminder 206 40))) (reminder (reminder 40 (reminder 206 40)) (reminder (reminder 206 40) (reminder 40 (reminder 206 40)))))
+; if #7
+; (reminder (reminder 206 40) (reminder 40 (reminder 206 40)))
+; 除了if条件部分调用14次外，仅最后有4次额外的reminder调用，合18次
+;
+; 应用序求值，reminder被调用4次
