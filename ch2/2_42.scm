2df5372f9ed7ebd41469368b120dbe0a50401dfd
dsh <dshbusiness@gmail.com>
2018-02-11 00:16:27 +0800

    x

--- ch2/2_42.scm
+++ ch2/2_42.scm
@@ -3,11 +3,80 @@
 
 ; 习题2.42
 
-(define (queens board-size)
-  (define (queen-cols k)
-    (if (= k 0)
-      (list empty-board)
-      (filter
-	(lambda (positions) (safe? k positions)
+(define (fold-right op init seq)
+  (if (null? seq)
+    init
+    (op (car seq) (fold-right op init (cdr seq)))
     )
   )
+
+(define (adjoin-position row board)
+  (cons row board)
+  )
+
+(define (enumerate beg end step)
+  (if (> beg end)
+    (list)
+    (cons beg (enumerate (+ beg step) end step))
+    )
+  )
+
+(define (add-rows boardset board-size)
+  (fold-right append (list) (map (lambda (oneboard)
+				   (map (lambda (i) (adjoin-position i oneboard))
+					(enumerate 1 board-size 1))
+				   ) boardset)
+	      )
+  )
+
+(define (check-row board)
+  (if (null? board)
+    #t
+    (null? (filter (lambda (x) (= x (car board))) (cdr board)))
+    )
+  )
+
+(define (check-dia-up k board)
+  (cond ((or (<= k 0) (null? board)) #t)
+	((= k (car board)) #f)
+	(else check-dia-up (- k 1) (cdr board))
+	)
+  )
+
+(define (check-dia-down k board)
+  (cond ((null? board) #t)
+	((= k (car board)) #f)
+	(else check-dia-up (+ k 1) (cdr board))
+	)
+  )
+
+(define (check-dia k board)
+  (define cur (car board))
+  (and (check-dia-up (- cur 1) (cdr board))
+       (check-dia-down (+ cur 1) (cdr board))
+       )
+  )
+
+(define (safe? k board)
+  (and
+    (check-row board)
+    (check-dia k board)
+    )
+  )
+
+(define (queens n)
+  (define (queen-col k)
+    (if (= k 0)
+      (list (list))
+      (filter (lambda (board) (safe? k board))
+	      (add-rows (queen-col (- k 1)) n)
+	      )
+      )
+    )
+  (queen-col n)
+  )
+
+(display (queens 5))
+(newline)
+
+; 习题2.43 略
---
+++ ch2/2_44.scm
@@ -0,0 +1,10 @@
+; 习题2.44
+
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let (smaller (up-split painter (- n 1)))
+      (below (beside smaller smaller) painter)
+      )
+    )
+  )
