09ec3a4911295bf12d0fe707119e36f014404219
dsh <dshbusiness@gmail.com>
2018-03-19 21:28:24 +0800

    update

--- ch3/3_17.scm
+++ ch3/3_17.scm
@@ -14,3 +14,35 @@
       ((eq? (car lst) x) #t)
       (else (do-check x (cdr lst)))))
   (do-check x checked))
+
+(define (count-pairs x)
+  (cond
+    ((not (pair? x)) 0)
+    ((if-checked x) 0)
+    (else (begin
+            (set-check x)
+            (+ (count-pairs (car x))
+               (count-pairs (cdr x))
+               1)))))
+
+(define x (cons 'a 'b))
+(define y (cons 'a 'b))
+(define z (cons x y))
+(set-car! x y)
+
+(display (count-pairs z))
+(newline)
+
+(define x (cons 'a 'b))
+(define y (cons x x))
+(define z (cons y y))
+(display (count-pairs z))
+(newline)
+
+(define x (cons 'a 'b))
+(define y (cons 'a 'b))
+(define z (cons x y))
+(set-cdr! x y)
+(set-cdr! y x)
+(display (count-pairs z))
+(newline)
