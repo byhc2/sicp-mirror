1da1b0d3a5e96f9d05b13fd2da758a34cc68055b
dsh <dshbusiness@gmail.com>
2018-04-16 10:09:08 +0800

    update

--- ch3/3_37.scm
+++ ch3/3_37.scm
@@ -1,6 +1,9 @@
 #!/usr/bin/guile
 !#
 
+(add-to-load-path ".")
+(load "constrains.scm")
+
 ; 习题3.37
 
 (define (c+ x y)
@@ -18,7 +21,25 @@
     (multiplier x y z)
     z))
 
-(define (c* x y)
+(define (c/ x y)
   (let ((z (make-connector)))
     (multiplier z y x)
     z))
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
+
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define C (make-connector))
+(probe "C temp" C)
+(define F (celsius-fahrenheit-converter C))
+
+(set-value! F 200 'user)
+
+(newline)
