9326d1423dac37c312778331034f0ea9f24d714b
byhc <3134706583@qq.com>
2018-08-15 14:36:02 +0800

    复习

--- ch3/3_73.scm
+++ ch3/3_73.scm
@@ -12,4 +12,17 @@
   int)
 
 (define x integers)
-(display-stream-n (integral x 2 0.1) 10)
+; (display-stream-n (integral x 2 0.1) 10)
+
+(define dt 0.0001)
+(define (step x)
+  (cons-stream x (step (+ x dt))))
+(define dt-series (stream-map (lambda (x y) (* x y)) (step 0) (step 0)))
+
+;(display-stream-n dt-series 10)
+
+; 此处以dt为步长，构造了0到1之间的x^2序列
+; 如0 0.0001^2 0.0002^2 ...
+; 然后调用integral计算其积分值
+; 随dt减小，序列最后一项无限趋近于1/3
+(display-stream-n (integral dt-series 0 dt) (/ 1 dt))
